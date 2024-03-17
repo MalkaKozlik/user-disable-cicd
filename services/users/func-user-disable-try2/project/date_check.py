0a61f434b59537a99c62d5932d783ec29af512f9
M. Kozlik <malkak@skyvar.co.il>
2024-03-17 14:18:46 +0200

    Doing all the code in comment

--- services/users/func-user-disable-try2/project/date_check.py
+++ services/users/func-user-disable-try2/project/date_check.py
@@ -1,12 +1,12 @@
 import datetime
 
-from project.update_user_attribute import update_user_attribute, remove_user_attribute
+# from project.update_user_attribute import update_user_attribute, remove_user_attribute
 
 
 def is_past_expiration_by(days: int, date, access_token, user_id, attribute_name):
     desired_date = adding_days_to_date(days, date)
     date = convert_datetime_to_date(desired_date)
-    disable_user_by_expiration(date, access_token, user_id, attribute_name)
+    # disable_user_by_expiration(date, access_token, user_id, attribute_name)
 
 
 def adding_days_to_date(days: int, date):
@@ -14,13 +14,13 @@ def adding_days_to_date(days: int, date):
     return required_date
 
 
-def disable_user_by_expiration(date, access_token, user_id, attribute_name):
-    if datetime.date.today() > date:
-        try:
-            update_user_attribute(access_token, user_id, "accountEnabled", "false")
-            remove_user_attribute(access_token, user_id, attribute_name)
-        except Exception:
-            return
+# def disable_user_by_expiration(date, access_token, user_id, attribute_name):
+#     if datetime.date.today() > date:
+#         try:
+#             update_user_attribute(access_token, user_id, "accountEnabled", "false")
+#             remove_user_attribute(access_token, user_id, attribute_name)
+#         except Exception:
+#             return
 
 
 def convert_datetime_to_date(date):
--- services/users/func-user-disable-try2/project/update_user_attribute.py
+++ services/users/func-user-disable-try2/project/update_user_attribute.py
@@ -1,5 +1,5 @@
-import requests
-import json
+# import requests
+# import json
 
 
 def update_user_attribute(access_token, user_id, attribute_name, attribute_value):
@@ -8,10 +8,10 @@ def update_user_attribute(access_token, user_id, attribute_name, attribute_value
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json",
     }
-    data = json.dumps({attribute_name: attribute_value})
-    response = requests.patch(url, headers=headers, data=data)
-    if response.status_code != 204:
-        raise Exception("The status code is: " + str(response.status_code))
+    # data = json.dumps({attribute_name: attribute_value})
+    # response = requests.patch(url, headers=headers, data=data)
+    # if response.status_code != 204:
+    #     raise Exception("The status code is: " + str(response.status_code))
 
 
 def remove_user_attribute(access_token, user_id, attribute_name):
